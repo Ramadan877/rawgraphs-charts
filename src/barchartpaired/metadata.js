68bfb67d136b117ff39f7e5a51933e90c7e8fa3c
David Egger <david.egga@gmx.at>
2022-05-27 20:29:59 +0200

    [barchartpaired] Add template for barchartpaired

---
+++ src/barchartpaired/metadata.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  name: 'Paired Bar Chart',
+  id: 'rawgraphs.barchartpaired',
+  // thumbnail,
+  // icon,
+  categories: ['Test', 'Debugging'],
+  description: 'An empty chart to bootstrap new ones',
+  // code: 'https://github.com/rawgraphs',
+  // tutorial: 'https://rawgraphs.io/learning/',
+}
