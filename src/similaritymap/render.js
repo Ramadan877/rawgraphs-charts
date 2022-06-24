11e2e2ee352e9f07617a3c62647e8b65428fb73d
David Egger <david.egga@gmx.at>
2022-06-24 22:13:09 +0200

    Implement similarity map render functionality

--- src/similaritymap/render.js
+++ src/similaritymap/render.js
@@ -1,5 +1,5 @@
 import * as d3 from 'd3'
-import {allTSNEE} from "rawcharts/tsne"
+import {allTSNEE} from "../tsne"
 import '../d3-styles.js'
 
 export function render(node, data, visualOptions, mapping) {
@@ -8,311 +8,143 @@ export function render(node, data, visualOptions, mapping) {
     // default options
     width,
     height,
+    marginLeft,
+    marginRight,
+    marginBottom,
+    marginTop,
     background,
     dotsRadius,
-    colorScale
+    colorScale,
+    title
   } = visualOptions
 
-  var opt = {}
-  opt.epsilon = 10; // epsilon is learning rate (10 = default)
-  opt.perplexity = 30; // roughly how many neighbors each point influences (30 = default)
-  opt.dim = 2; // dimensionality of the embedding (2 = default)
+  const {
+    titleSize,
+    boundWidth,
+    boundHeight,
+    boundLeft,
+    boundTop,
+    xAccessor,
+    yAccessor,
+    reducedDimensions
+  } = calcProps()
+  const xDimension = reducedDimensions.map(point => point[0])
+  const yDimension = reducedDimensions.map(point => point[1])
 
-  var tsne =  allTSNEE(opt) // create a tSNE instance
+  const svg = d3.select(node)
+  const bounds = createBounds()
+  const {xScale, yScale} = createScales()
+  const {xAxis, yAxis} = createAxes()
+  const {dots} = drawScatterPoints()
 
-  var iris = [
-    [5.1,3.5,1.4,0.2],
-    [4.9,3.0,1.4,0.2],
-    [4.7,3.2,1.3,0.2],
-    [4.6,3.1,1.5,0.2],
-    [5.0,3.6,1.4,0.2],
-    [5.4,3.9,1.7,0.4],
-    [4.6,3.4,1.4,0.3],
-    [5.0,3.4,1.5,0.2],
-    [4.4,2.9,1.4,0.2],
-    [4.9,3.1,1.5,0.1],
-    [5.4,3.7,1.5,0.2],
-    [4.8,3.4,1.6,0.2],
-    [4.8,3.0,1.4,0.1],
-    [4.3,3.0,1.1,0.1],
-    [5.8,4.0,1.2,0.2],
-    [5.7,4.4,1.5,0.4],
-    [5.4,3.9,1.3,0.4],
-    [5.1,3.5,1.4,0.3],
-    [5.7,3.8,1.7,0.3],
-    [5.1,3.8,1.5,0.3],
-    [5.4,3.4,1.7,0.2],
-    [5.1,3.7,1.5,0.4],
-    [4.6,3.6,1.0,0.2],
-    [5.1,3.3,1.7,0.5],
-    [4.8,3.4,1.9,0.2],
-    [5.0,3.0,1.6,0.2],
-    [5.0,3.4,1.6,0.4],
-    [5.2,3.5,1.5,0.2],
-    [5.2,3.4,1.4,0.2],
-    [4.7,3.2,1.6,0.2],
-    [4.8,3.1,1.6,0.2],
-    [5.4,3.4,1.5,0.4],
-    [5.2,4.1,1.5,0.1],
-    [5.5,4.2,1.4,0.2],
-    [4.9,3.1,1.5,0.1],
-    [5.0,3.2,1.2,0.2],
-    [5.5,3.5,1.3,0.2],
-    [4.9,3.1,1.5,0.1],
-    [4.4,3.0,1.3,0.2],
-    [5.1,3.4,1.5,0.2],
-    [5.0,3.5,1.3,0.3],
-    [4.5,2.3,1.3,0.3],
-    [4.4,3.2,1.3,0.2],
-    [5.0,3.5,1.6,0.6],
-    [5.1,3.8,1.9,0.4],
-    [4.8,3.0,1.4,0.3],
-    [5.1,3.8,1.6,0.2],
-    [4.6,3.2,1.4,0.2],
-    [5.3,3.7,1.5,0.2],
-    [5.0,3.3,1.4,0.2],
-    [7.0,3.2,4.7,1.4],
-    [6.4,3.2,4.5,1.5],
-    [6.9,3.1,4.9,1.5],
-    [5.5,2.3,4.0,1.3],
-    [6.5,2.8,4.6,1.5],
-    [5.7,2.8,4.5,1.3],
-    [6.3,3.3,4.7,1.6],
-    [4.9,2.4,3.3,1.0],
-    [6.6,2.9,4.6,1.3],
-    [5.2,2.7,3.9,1.4],
-    [5.0,2.0,3.5,1.0],
-    [5.9,3.0,4.2,1.5],
-    [6.0,2.2,4.0,1.0],
-    [6.1,2.9,4.7,1.4],
-    [5.6,2.9,3.6,1.3],
-    [6.7,3.1,4.4,1.4],
-    [5.6,3.0,4.5,1.5],
-    [5.8,2.7,4.1,1.0],
-    [6.2,2.2,4.5,1.5],
-    [5.6,2.5,3.9,1.1],
-    [5.9,3.2,4.8,1.8],
-    [6.1,2.8,4.0,1.3],
-    [6.3,2.5,4.9,1.5],
-    [6.1,2.8,4.7,1.2],
-    [6.4,2.9,4.3,1.3],
-    [6.6,3.0,4.4,1.4],
-    [6.8,2.8,4.8,1.4],
-    [6.7,3.0,5.0,1.7],
-    [6.0,2.9,4.5,1.5],
-    [5.7,2.6,3.5,1.0],
-    [5.5,2.4,3.8,1.1],
-    [5.5,2.4,3.7,1.0],
-    [5.8,2.7,3.9,1.2],
-    [6.0,2.7,5.1,1.6],
-    [5.4,3.0,4.5,1.5],
-    [6.0,3.4,4.5,1.6],
-    [6.7,3.1,4.7,1.5],
-    [6.3,2.3,4.4,1.3],
-    [5.6,3.0,4.1,1.3],
-    [5.5,2.5,4.0,1.3],
-    [5.5,2.6,4.4,1.2],
-    [6.1,3.0,4.6,1.4],
-    [5.8,2.6,4.0,1.2],
-    [5.0,2.3,3.3,1.0],
-    [5.6,2.7,4.2,1.3],
-    [5.7,3.0,4.2,1.2],
-    [5.7,2.9,4.2,1.3],
-    [6.2,2.9,4.3,1.3],
-    [5.1,2.5,3.0,1.1],
-    [5.7,2.8,4.1,1.3],
-    [6.3,3.3,6.0,2.5],
-    [5.8,2.7,5.1,1.9],
-    [7.1,3.0,5.9,2.1],
-    [6.3,2.9,5.6,1.8],
-    [6.5,3.0,5.8,2.2],
-    [7.6,3.0,6.6,2.1],
-    [4.9,2.5,4.5,1.7],
-    [7.3,2.9,6.3,1.8],
-    [6.7,2.5,5.8,1.8],
-    [7.2,3.6,6.1,2.5],
-    [6.5,3.2,5.1,2.0],
-    [6.4,2.7,5.3,1.9],
-    [6.8,3.0,5.5,2.1],
-    [5.7,2.5,5.0,2.0],
-    [5.8,2.8,5.1,2.4],
-    [6.4,3.2,5.3,2.3],
-    [6.5,3.0,5.5,1.8],
-    [7.7,3.8,6.7,2.2],
-    [7.7,2.6,6.9,2.3],
-    [6.0,2.2,5.0,1.5],
-    [6.9,3.2,5.7,2.3],
-    [5.6,2.8,4.9,2.0],
-    [7.7,2.8,6.7,2.0],
-    [6.3,2.7,4.9,1.8],
-    [6.7,3.3,5.7,2.1],
-    [7.2,3.2,6.0,1.8],
-    [6.2,2.8,4.8,1.8],
-    [6.1,3.0,4.9,1.8],
-    [6.4,2.8,5.6,2.1],
-    [7.2,3.0,5.8,1.6],
-    [7.4,2.8,6.1,1.9],
-    [7.9,3.8,6.4,2.0],
-    [6.4,2.8,5.6,2.2],
-    [6.3,2.8,5.1,1.5],
-    [6.1,2.6,5.6,1.4],
-    [7.7,3.0,6.1,2.3],
-    [6.3,3.4,5.6,2.4],
-    [6.4,3.1,5.5,1.8],
-    [6.0,3.0,4.8,1.8],
-    [6.9,3.1,5.4,2.1],
-    [6.7,3.1,5.6,2.4],
-    [6.9,3.1,5.1,2.3],
-    [5.8,2.7,5.1,1.9],
-    [6.8,3.2,5.9,2.3],
-    [6.7,3.3,5.7,2.5],
-    [6.7,3.0,5.2,2.3],
-    [6.3,2.5,5.0,1.9],
-    [6.5,3.0,5.2,2.0],
-    [6.2,3.4,5.4,2.3],
-    [5.9,3.0,5.1,1.8]]
+  function calcProps() {
+    const minTitleHeight = 300
+    const titleSize = height / 30
 
-  tsne.initDataRaw(iris)
+    let boundWidth = width - marginLeft - marginRight
+    let boundHeight = height - marginTop - marginBottom
+    let boundLeft = marginLeft
+    let boundTop =  boundHeight >= minTitleHeight ? marginTop + titleSize : marginTop
 
-  for(var k = 0; k < 500; k++) {
-    tsne.step(); // every time you call this, solution gets better
-  }
-
-  var Y = tsne.getSolution(); // Y is an array of 2-D points that you can plot
-
-  console.log("printing Y: ")
-  console.log(Y)
-
-  drawScatter(Y);
-
-  function drawScatter(data) {
-    //access data
-    //const dataset = await d3.json("/my_weather_data.json");
-    const dataset = data
-    console.table(dataset[0]);
-
-    //const xAccessor = d => d.dewPoint
-    //const yAccessor = d => d.humidity
+    if (boundHeight >= minTitleHeight) {
+      boundHeight -= titleSize
+    }
 
     const xAccessor = d => d[0]
     const yAccessor = d => d[1]
 
+    const reducedDimensions = calcReducedDimensions()
 
-    //chart dimensions
-    const width = d3.min([
-      window.innerWidth * 0.9,
-      window.innerHeight * 0.9,
-    ])
+    return {minTitleHeight, titleSize, boundWidth, boundHeight, boundLeft, boundTop,
+      xAccessor, yAccessor, reducedDimensions}
+  }
 
-    let dimensions = {
-      width: width,
-      height: width,
-      margin:{
-        top: 10,
-        right: 10,
-        bottom: 50,
-        left: 50,
-      },
+  function calcReducedDimensions() {
+    var opt = {}
+    opt.epsilon = 10; // epsilon is learning rate (10 = default)
+    opt.perplexity = 30; // roughly how many neighbors each point influences (30 = default)
+    opt.dim = 2; // dimensionality of the embedding (2 = default)
+
+    var tsne =  allTSNEE(opt)
+    const tsneData = data.map(row => {
+      return row.dimensions
+    })
+
+    tsne.initDataRaw(tsneData)
+
+    for(var k = 0; k < 500; k++) {
+      tsne.step(); // every time you call this, solution gets better
     }
 
-    dimensions.boundedWidth = dimensions.width
-        - dimensions.margin.left
-        - dimensions.margin.right;
-    dimensions.boundedHeight = dimensions.height
-        - dimensions.margin.top
-        - dimensions.margin.bottom;
+    return tsne.getSolution(); // Y is an array of 2-D points that you can plot
+  }
 
-    //wrapper and bounds
-    const wrapper = d3.select(node)
-        .append("svg")
-        .attr("width", dimensions.width)
-        .attr("height", dimensions.height);
+  function createBounds() {
+    svg.append('rect')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('fill', background)
 
-    const bounds = wrapper.append("g")
-        .style("transform", `translate(${
-            dimensions.margin.left
-        }px, ${
-            dimensions.margin.top
-        }px)`);
+    if (titleSize) {
+      svg.append('text')
+          .text(title)
+          .attr('x', width / 2)
+          .attr('y', marginTop)
+          .style("text-anchor", "middle")
+          .attr("font-size", titleSize)
+    }
 
-    //creating x scale
-    const xScale = d3.scaleLinear()
-        .domain(d3.extent(dataset, xAccessor))
-        .range([0, dimensions.boundedWidth])
+    return svg.append("g")
+        .attr("transform", `translate(
+      ${boundLeft},
+      ${boundTop})`)
+  }
 
-    xScale.nice()
-
-    //creating y scale
-    const yScale = d3.scaleLinear()
-        .domain(d3.extent(dataset,yAccessor))
-        .range([dimensions.boundedHeight,0])
+  function createScales() {
+    const xScale = d3
+        .scaleLinear()
+        .domain(d3.extent(xDimension))
+        .range([0, boundWidth])
         .nice()
 
-    //drawing the dots
-    /* this method would be inefficent upon multiple calls as it always redraws everything
-    dataset.forEach(d => {
-      bounds
-          .append("circle")
-          .attr("cx", xScale(xAccessor(d)))
-          .attr("cy", yScale(yAccessor(d)))
-          .attr("r",5)
+    const yScale = d3
+        .scaleLinear()
+        .domain(d3.extent(yDimension))
+        .range([boundHeight, 0])
 
-    })
-    */
-    /*temp disable
-      const dots = bounds.selectAll("cirlce")
-          .data(dataset)
-          .enter().append("circle")
-          .attr("cx", d => xScale(xAccessor(d)))
-          .attr("cy", d => yScale(yAccessor(d)))
-          .attr("r", 5)
-          .attr("fill", "cornflowerblue")
-    */
+    return {xScale, yScale}
+  }
 
-    function drawDots(dataset, color) {
-      const dots = bounds.selectAll("circle").data(dataset)
-      dots.join("circle")
-          .attr("cx", d => xScale(xAccessor(d)))
-          .attr("cy", d => yScale(yAccessor(d)))
-          .attr("r", 5)
-          .attr("fill", color)
-    }
-
-    drawDots(dataset.slice(0,200), "darkgrey")
-
-    setTimeout(() => {
-      drawDots(dataset, "cornflowerblue")
-    }, 1000)
-
-
-    //create x axis labels
-    const xAxisGenerator = d3.axisBottom().scale(xScale)
-    const xAxis = bounds.append("g")
-        .call(xAxisGenerator)
-        .style("transform", `translateY(${dimensions.boundedHeight}px)`)
-
-    //label for x axisBottom
-    const xAxisLabel = xAxis.append("text")
-        .attr("x", dimensions.boundedWidth/2)
-        .attr("y", dimensions.margin.bottom - 10)
-        .attr("fill", "black")
-        .style("font-size", "1.4em")
-        //.html("Dew point (&deg;F)")
-        .html("Similarity Iris Dataset ")
-
-    //create y axis labels
-    const yAxisGenerator = d3.axisLeft().scale(yScale).ticks(4)
+  function createAxes() {
+    const yAxisGenerator = d3.axisLeft()
+        .scale(yScale)
     const yAxis = bounds.append("g")
         .call(yAxisGenerator)
+        .attr("text-anchor", "left")
+    yAxis.attr("transform", `translate(${0}, 0)`)
 
-    const yAxisLabel = yAxis.append("text")
-        .attr("x", -dimensions.boundedHeight / 2)
-        .attr("y", -dimensions.margin.left + 10)
-        .attr("fill", "black")
-        .style("font-size", "1.4em")
-        .style("transform", "rotate(-90deg)")
-        .style("text-anchor", "middle")
-    //.text("Relative humidity")
+    yAxis.selectAll("text")
+        // .attr("text-anchor", labelLeftAlignment) //TODO make good visualization
+        .attr("transform", `translate(${40}, 0)`)
 
-  }//end draw scatterplot function
+    const xAxisGenerator = d3.axisBottom()
+        .scale(xScale)
+    const xAxis = bounds.append("g")
+        .call(xAxisGenerator)
+        .attr("transform",
+            `translate(${0}, ${boundHeight})`)
+
+    return({xAxis, yAxis})
+  }
+
+  function drawScatterPoints() {
+        const dots = bounds.selectAll("circle").data(reducedDimensions)
+        dots.join("circle")
+            .attr("cx", d => xScale(xAccessor(d)))
+            .attr("cy", d => yScale(yAccessor(d)))
+            .attr("r", 5)
+            // .attr("fill", color)
+            .attr("fill", "#0365a8")
+    return dots
+  }
 }
