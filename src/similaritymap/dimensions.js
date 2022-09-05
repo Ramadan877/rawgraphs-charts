a87ef2083ec31a9f8509eb2641713e876903ada2
David Egger <david.egga@gmx.at>
2022-09-05 15:32:29 +0200

    Add Tick Label Visibility Option, Add Hover Label

--- src/similaritymap/dimensions.js
+++ src/similaritymap/dimensions.js
@@ -1,5 +1,12 @@
 export const dimensions = [
   {
+    id: "labels",
+    name: 'Hover Labels',
+    validTypes: ['string', 'number'],
+    required: false,
+  },
+
+  {
     id: 'classification',
     name: 'Classification',
     validTypes: ['string', 'number'],
